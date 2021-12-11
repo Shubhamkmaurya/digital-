02c9adea380d4a6ff5f0074d2fce72864685bb35
Shubhamkmaurya <93081549+Shubhamkmaurya@users.noreply.github.com>
2021-12-11 20:18:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,7 @@
 import React {Component, useState} form "react";
 import React from 'react'
-const App = () => {
 
-//When Digital Clock is mounted, the Digital Clock should update the state variable time with the current time.
-//This Digital Clock should update the time variable regularly at an interval of 1 sec.
-//When the component is unmounted then Digital Clock should be terminated to free up resources.
-
-//Date time should be displayed inside div with classname "date-time"
-//Use JavaScript class Date.
-//Update Digital Clock in every second.
-//Terminate Digital Clock on unmounting.
-
-//use toLocaleString method on date object to display date-time
-//It should be displayed like this: - "12/2/2021, 3:17:23 PM" (without double quotes)
-//On some OS/browsers , toLocaleString may or may not show AM/PM at the end, but the solution will still be acceptable  
+ 
   
   class App extends Component {
     constructor() {
@@ -37,7 +25,7 @@ const App = () => {
        </div>
   );
 }
-}
+
  
 
 export default App;
